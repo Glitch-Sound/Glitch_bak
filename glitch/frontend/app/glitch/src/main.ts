97c17566b1f7f057e6b33fc00a3f8ea6686228ec
Glitch-Sound <uid.glitch@gmail.com>
2024-08-10 16:31:11 +0900

    frontend

--- glitch/frontend/app/glitch/src/main.ts
+++ glitch/frontend/app/glitch/src/main.ts
@@ -8,20 +8,45 @@ import router from '@/router'
 
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import WebFont from 'webfontloader'
+import './assets/main.css'
+
 const app = createApp(App)
 const pinia = createPinia()
+
+WebFont.load({
+  google: {
+    families: ['Noto+Sans+JP:wght@100..900&display=swap']
+  }
+})
+
+const myDarkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: '#000000',
+    surface: '#0a0a0a',
+    'on-surface': '#cdcdcd'
+  },
+  variables: {
+    fontFamily: 'Noto Sans JP, sans-serif'
+  }
+}
 
 const vuetify = createVuetify({
   components,
   directives,
   theme: {
+    defaultTheme: 'myDarkTheme',
     // defaultTheme: 'dark'
-    defaultTheme: 'light'
+    // defaultTheme: 'light'
+    themes: {
+      myDarkTheme
+    }
   },
   icons: {
     defaultSet: 'mdi',
