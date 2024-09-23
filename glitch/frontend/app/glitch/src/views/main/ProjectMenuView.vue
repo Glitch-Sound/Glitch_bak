426d15cc9aadd11e69976f26e4038b890dd89a20
Glitch-Sound <uid.glitch@gmail.com>
2024-09-24 07:16:34 +0900

    frontend

--- glitch/frontend/app/glitch/src/views/main/ProjectMenuView.vue
+++ glitch/frontend/app/glitch/src/views/main/ProjectMenuView.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
-import { ItemType, type EventCreate } from '@/types/Item'
+import { ItemType, ExtractType, type EventCreate } from '@/types/Item'
 import useUserStore from '@/stores/UserStore'
 import useItemStore from '@/stores/ItemStore'
 import CreateEventDialog from '@/components/dialog/CreateEventDialog.vue'
@@ -45,28 +45,72 @@ const handleSubmit = async (data: EventCreate) => {
         </v-btn>
       </v-list-item>
 
-      <v-list-item @click="store_item.setExtractIncomplete()">
+      <v-list-item
+        :class="{
+          'extract-selected': store_item.type_extract === ExtractType.INCOMPLETE,
+          'extract-unselected': store_item.type_extract !== ExtractType.INCOMPLETE
+        }"
+        @click="store_item.setExtractIncomplete()"
+      >
         <v-icon icon="mdi-moon-waning-crescent" />Incomplete
       </v-list-item>
-      <v-list-item @click="store_item.setExtractAll()">
+
+      <v-list-item
+        :class="{
+          'extract-selected': store_item.type_extract === ExtractType.ALL,
+          'extract-unselected': store_item.type_extract !== ExtractType.ALL
+        }"
+        @click="store_item.setExtractAll()"
+      >
         <v-icon icon="mdi-moon-full" />All
       </v-list-item>
-      <v-list-item @click="store_item.setExtractHighRisk()">
+
+      <v-list-item
+        :class="{
+          'extract-selected': store_item.type_extract === ExtractType.HIGH_RISK,
+          'extract-unselected': store_item.type_extract !== ExtractType.HIGH_RISK
+        }"
+        @click="store_item.setExtractHighRisk()"
+      >
         <v-icon icon="mdi-fire" />High Risk
       </v-list-item>
-      <v-list-item @click="store_item.setExtractAlert()">
+
+      <v-list-item
+        :class="{
+          'extract-selected': store_item.type_extract === ExtractType.ALERT,
+          'extract-unselected': store_item.type_extract !== ExtractType.ALERT
+        }"
+        @click="store_item.setExtractAlert()"
+      >
         <v-icon icon="mdi-alert-box" />Alert
       </v-list-item>
-      <v-list-item @click="store_item.setExtractAssignment()">
+
+      <v-list-item
+        :class="{
+          'extract-selected': store_item.type_extract === ExtractType.ASSIGNMENT,
+          'extract-unselected': store_item.type_extract !== ExtractType.ASSIGNMENT
+        }"
+        @click="store_item.setExtractAssignment()"
+      >
         <v-icon icon="mdi-account" />Assignment
       </v-list-item>
+
       <v-list-item
+        :class="{
+          'extract-selected': store_item.type_extract === ExtractType.SEARCH,
+          'extract-unselected': store_item.type_extract !== ExtractType.SEARCH
+        }"
         @click="store_item.setExtractSearchUpdate()"
         :disabled="store_item.extract_search_target === ''"
       >
         <v-icon icon="mdi-magnify" />Search
       </v-list-item>
+
       <v-list-item
+        :class="{
+          'extract-selected': store_item.type_extract === ExtractType.RELATION,
+          'extract-unselected': store_item.type_extract !== ExtractType.RELATION
+        }"
         @click="store_item.setExtractItemUpdate()"
         :disabled="store_item.extract_rid_item === 0"
       >
@@ -127,5 +171,13 @@ const handleSubmit = async (data: EventCreate) => {
 
 .v-icon {
   margin: 0 15px 0 0;
+}
+
+.extract-selected {
+  border-left: 4px solid #196fb3;
+}
+
+.extract-unselected {
+  border-left: 4px solid transparent;
 }
 </style>
